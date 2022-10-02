d2f8b3acf9facf126208a4af72d264e1f0f31270
AlbertoSimula <103315974+AlbertoSimula@users.noreply.github.com>
2022-10-02 20:53:26 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+let myVar = 3
+myVar += 10
+console.log(myVar)
+
+interface MyInterface{
+    myNumber:number;
+    myString:string;
+}
+let myInterface : MyInterface = {
+    myNumber:10,
+    myString:"Hello World",
+}
+console.log(myInterface.myNumber);
+console.log(myInterface.myString);
